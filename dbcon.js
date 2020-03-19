8ce0f037921618a2110e005f6a93f251dcf947d3
grigorisOSU <50174938+grigorisOSU@users.noreply.github.com>
2020-03-19 13:08:19 -0700

    Delete dbcon.js

--- dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_grigoris',
-  password        : '9573',
-  database        : 'cs340_grigoris'
-});
-module.exports.pool = pool;
